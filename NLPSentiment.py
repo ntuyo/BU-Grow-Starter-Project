8b447e9c5aa345115724cb2621e6b3bfc5347617
Ccat12 <cathleenchen12@gmail.com>
2019-06-28 11:36:31 -0400

    Update NLPSentiment.py

--- NLPSentiment.py
+++ NLPSentiment.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.model_selection import train_test_split # function for splitting data to train and test sets
@@ -6,14 +7,22 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.classify import SklearnClassifier
 
-from wordcloud import WordCloud,STOPWORDS
 import matplotlib.pyplot as plt
-%matplotlib inline
+'exec(%matplotlib inline)'
 from subprocess import check_output
 
-
-data = pd.read_csv('../input/Sentiment_GOP.csv')
-# Keeping only the neccessary columns
+input_path = os.path.dirname(os.path.realpath(__file__))+'/input/Sentiment_GOP.csv'
+data = pd.read_csv(input_path, encoding = "ISO-8859-1")
 data = data[['text','sentiment']]
 
+# Splitting the dataset into train and test set
+train, test = train_test_split(data,test_size = 0.1)
+# Removing neutral sentiments
+train = train[train.sentiment != "Neutral"]
 
+train_pos = train[ train['sentiment'] == 'Positive']
+train_pos = train_pos['text']
+train_neg = train[ train['sentiment'] == 'Negative']
+train_neg = train_neg['text']
+
+print(train_neg)
